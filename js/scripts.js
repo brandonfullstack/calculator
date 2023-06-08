faac2f562f7c8060e7678c77b298d2569d1d1438
Brandon Wright <brandonfullstack@gmail.com>
2023-06-07 23:15:13 -0400

    Combine window.alert() method calls

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,23 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1,number2));
+window.alert("The sum is " + add(number1,number2) + ".\n" +
+"The difference is " + subtract(number1,number2) + ".\n" +
+"The product is " + multiply(number1,number2) + ".\n" +
+"The quotient is " + divide(number1,number2) + ".");
