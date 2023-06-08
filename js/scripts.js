44713bba3f5bfb105b2b14b7ce951cc9c9bd628e
Brandon Wright <brandonfullstack@gmail.com>
2023-06-07 23:37:06 -0400

    Add user's inputted numbers

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The sum is " + add(number1,number2) + ".\n" +
-"The difference is " + subtract(number1,number2) + ".\n" +
-"The product is " + multiply(number1,number2) + ".\n" +
-"The quotient is " + divide(number1,number2) + ".");
+window.alert("The sum of " + number1 + " and " + number2 + " is " + add(number1,number2) + ".\n" +
+"The difference between " + number1 + " and " + number2 + " is " + subtract(number1,number2) + ".\n" +
+"The product of " + number1 + " and " + number2 + " is " + multiply(number1,number2) + ".\n" +
+"The quotient of " + number1 + " and " + number2 + " is " + divide(number1,number2) + ".");
